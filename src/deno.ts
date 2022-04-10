ae7a98355f4a5a256cc121ae59ba6d12be16556d
CGQAQ <m.jason.liu@outlook.com>
2022-04-10 19:11:34 +0800

    add configs

--- src/deno.ts
+++ src/deno.ts
@@ -1,5 +1,15 @@
 import { run, runWithStdio } from "./run";
 
+let _enableFormatter = true;
+
+export function disableFormatter() {
+  _enableFormatter = false;
+}
+
+export function enableFormatter() {
+  _enableFormatter = true;
+}
+
 export function formatFile(...files: string[]) {
   run("deno", "fmt", ...files);
 }
